a59abc899b953a597db9341d9d7d3d6b6308fe55
Annge001 <anngela0222@gmail.com>
2023-01-19 19:25:47 -0300

    up

--- Entregas/preEntrega2Perez/index2.js
+++ Entregas/preEntrega2Perez/index2.js
@@ -1,19 +1,26 @@
-function calcularIMC() {
+function validarIMC(imc) {
+    if (imc < 18.5) {
+        return 'puede que estés muy delgado';
+    } else if (imc < 25) {
+        return 'al parecer estás saludable';
+    } else {
+        return 'puede que tengas sobrepeso';
+    }
+}
+
+function calcularIMC(pesoEnKilos, alturaEnMetros) {
+    var imc = pesoEnKilos / (alturaEnMetros * alturaEnMetros);
+    return imc;
+}
+
+function IMC() {
     var nombre = document.getElementById('nombre').value;
     var alturaEnCentimetros = parseInt(document.getElementById('altura').value);
     var alturaEnMetros = alturaEnCentimetros / 100;
     var pesoEnKilos = parseInt(document.getElementById('peso').value);
-
-    var imc = pesoEnKilos / (alturaEnMetros * alturaEnMetros);
+    var imc = calcularIMC(pesoEnKilos, alturaEnMetros);
     var clasificacion;
-
-    if (imc < 18.5) {
-        clasificacion = 'puede que estés muy delgado';
-    } else if (imc < 25) {
-        clasificacion = 'al parecer estás saludable';
-    } else {
-        clasificacion = 'puede que tengas sobrepeso';
-    }
+    clasificacion = validarIMC(imc);
 
     var respuesta = 'Hola, tu IMC es ' + imc + ' y  ' + clasificacion;
     alert(respuesta);
